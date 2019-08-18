53e7d2705d87148274b5321276b27e3130a87a33
Guy Pensart <guy.pensart@hotmail.com>
2019-08-18 15:33:37 +0200

    Checkbox to mark todoItem complete

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -2,13 +2,39 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 export class TodoItem extends Component {
+    
+    getStyle = () => {
+        return {
+            background: '#F4F4F4',
+            padding: '10px',
+            borderBottom: '1px #CCC dotted',
+            textDecoration: this.props.todo.completed? 'line-through' : 'none'
+        }
+    }
+
     render() {
+        const { id, title } = this.props.todo;
         return (
-            <div>
-                <p>{ this.props.todo.title }</p>
+            <div style={this.getStyle()}>
+                <p>
+                    <input type="checkbox" defaultChecked={this.props.todo.completed} onChange={this.props.markComplete.bind(this, id)} />
+                    &nbsp;
+                    { title }
+                    <button style={btnStyle}>X</button>
+                </p>
             </div>
         )
     }
+}
+
+const btnStyle = {
+    background: '#FF0000',
+    color: '#FFF',
+    border: 'none',
+    padding: '5px 10px',
+    borderRadius: '3px',
+    cursor: 'pointer',
+    float: 'right'
 }
 
 TodoItem.propTypes = {
