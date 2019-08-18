df4a24ebb7d0fc3f38707df5f84d96a1ce2da8a3
Guy Pensart <guy.pensart@hotmail.com>
2019-08-18 13:25:32 +0200

    List of static array data

--- src/App.js
+++ src/App.js
@@ -1,15 +1,34 @@
-import React from 'react';
+import React, { Component } from 'react';
+import Todos from './components/Todos';
 import './App.css';
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
+class App extends Component {
+
+  state = {
+    todos: [
+      {
+        id: 1,
+        title: 'Finish this tutorial',
+        completed: false
+      },
+      {
+        id: 2,
+        title: 'Prepare meal',
+        completed: false
+      },
+      {
+        id: 3,
+        title: 'Call your best friend',
+        completed: true
+      }
+    ]
   }
 
   render() {
+    console.log(this.state.todos);
     return (
       <div className="App">
-        <h1>React Toto App</h1>
+        <Todos todos={this.state.todos} />
       </div>
     );
   }
