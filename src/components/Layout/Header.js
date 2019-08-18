895083cec2dea42b429c02ed783664c5536e9130
Guy Pensart <guy.pensart@hotmail.com>
2019-08-18 16:51:17 +0200

    Routing

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import {Link} from 'react-router-dom'
 
 function Header() {
     return (
         <header style={headerStyle} >
             <h1>React Todo List</h1>
+            <Link to="/" style={linkStyle} >Home</Link> | <Link to="/about" style={linkStyle} >About</Link>
         </header>
     )
 }
@@ -13,6 +15,11 @@ const headerStyle = {
     color: '#FFF',
     textAlign: 'center',
     padding: '10px'
+}
+
+const linkStyle = {
+    color: '#FFF',
+    textDecoration: 'none'
 }
 
 export default Header;
